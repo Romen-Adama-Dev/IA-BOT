8f066a1e084eef399df0abd409069de23489750d
Romen-Adama-Dev <romen_adama.c.r@outlook.com>
2025-09-24 16:59:22 +0100

    feat: improve language service and update app component for better internationalization support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component, signal, inject } from '@angular/core'
+import { Component, signal } from '@angular/core'
 import { TranslateModule } from '@ngx-translate/core'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatButtonToggleModule, MAT_BUTTON_TOGGLE_DEFAULT_OPTIONS } from '@angular/material/button-toggle'
 import { MatIconModule } from '@angular/material/icon'
 import { LiquidEtherComponent } from './liquid-ether/liquid-ether.component'
 import { LangService } from './services/lang.service'
@@ -11,9 +11,20 @@ import { LangService } from './services/lang.service'
   standalone: true,
   imports: [TranslateModule, MatToolbarModule, MatButtonToggleModule, MatIconModule, LiquidEtherComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [
+    {
+      provide: MAT_BUTTON_TOGGLE_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'standard',
+        hideSingleSelectionIndicator: true,
+        hideMultipleSelectionIndicator: true,
+        disabledInteractive: false
+      }
+    }
+  ]
 })
 export class AppComponent {
   protected readonly title = signal('ia-bot')
-  readonly lang = inject(LangService)
+  constructor(public lang: LangService) {}
 }
--- src/app/services/lang.service.ts
+++ src/app/services/lang.service.ts
@@ -1,19 +1,31 @@
 import { Injectable, inject, PLATFORM_ID } from '@angular/core'
-import { isPlatformBrowser } from '@angular/common'
+import { isPlatformBrowser, DOCUMENT } from '@angular/common'
 import { TranslateService } from '@ngx-translate/core'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({ providedIn: 'root' })
 export class LangService {
   private readonly platformId = inject(PLATFORM_ID)
+  private readonly doc = inject(DOCUMENT)
   private readonly storageKey = 'lang'
   readonly langs = ['es','en']
-  constructor(private t: TranslateService){
+  readonly lang$ = new BehaviorSubject<string>('es')
+
+  constructor(private readonly t: TranslateService){
     this.t.addLangs(this.langs)
     const saved = this.getSaved() ?? 'es'
     this.use(saved)
   }
-  use(lang: string){ this.t.use(lang); this.save(lang) }
+
+  use(lang: string){
+    this.t.use(lang)
+    this.lang$.next(lang)
+    this.save(lang)
+    if (isPlatformBrowser(this.platformId)) this.doc.documentElement.lang = lang
+  }
+
   current(){ return this.t.currentLang || this.t.defaultLang || 'es' }
+
   private save(lang: string){
     if (!isPlatformBrowser(this.platformId)) return
     localStorage.setItem(this.storageKey, lang)
