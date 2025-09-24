90959aeb7a5af4bf8c4e7c5b5af9ccf8fe438717
Romen-Adama-Dev <romen_adama.c.r@outlook.com>
2025-09-24 16:18:10 +0100

    feat: add LiquidEtherComponent with WebGL simulation and styling
    - Implemented LiquidEtherComponent for rendering a liquid simulation using Three.js.
    - Added HTML and SCSS files for the new component.
    - Updated app component to include LiquidEtherComponent.
    - Introduced global styles for better layout and responsiveness.
    - Added English and Spanish localization files for app title.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LiquidEtherComponent } from "./liquid-ether/liquid-ether.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LiquidEtherComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
